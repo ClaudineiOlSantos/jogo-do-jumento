6291f97cdc22dffd7eedea37669faafd352545be
claudinei <diney_rock19@hotmail.com>
2019-04-21 23:43:28 -0300

    Add menu

--- src/jumento/oh/JumentoOh.java
+++ src/jumento/oh/JumentoOh.java
@@ -3,6 +3,8 @@
  */
 package jumento.oh;
 
+import java.util.Scanner;
+
 /**
  *
  * @author claudinei
@@ -14,13 +16,62 @@ public class JumentoOh {
      */
     public static void main(String[] args) {
 
-        Duelo duelo = new Duelo(new Jumento("Jumento Celestino", 8, 1000, 500),
-                new Jumento("Jumento Bengala", 12, 1000, 500));
+        Duelo duelo;
+        Jumento j1, j2;
 
-        System.out.println(duelo.apresentacaoJogadores());
-        System.out.println(duelo.iniciaDuelo());
+        Scanner input = new Scanner(System.in);
+        int opcao = 0;
 
-        System.out.println(duelo.apresentacaoJogadores());
+        String nome;
+        int idade;
+        //Inicia player 1
+        System.out.println("Informe o nome do jumento 1.");
+        nome = input.next();
+        System.out.println("Informe a idade do jumento 1.");
+        idade = input.nextInt();
+
+        j1 = new Jumento(nome, idade, 1000, 500);
+
+        //Inicia player 2
+        System.out.println("Informe o nome do jumento 2.");
+        nome = input.next();
+        System.out.println("Informe a idade do jumento 2.");
+        idade = input.nextInt();
+
+        j2 = new Jumento(nome, idade, 1000, 500);
+
+        //apresenta os jogadores
+        System.out.println(j1);
+        System.out.println(j2);
+
+        do {
+
+            System.out.println("|------------------------------|");
+            System.out.println("|***Escolha a opção desejada***|");
+            System.out.println("|------------------------------|");
+            System.out.println("|1 - Duelar                    |");
+            System.out.println("|0 - sair                      |");
+            System.out.println("|------------------------------|");
+
+            System.out.println("O que deseja fazer?");
+            opcao = (input.nextInt());
+
+            switch (opcao) {
+                case 1:
+                    duelo = new Duelo(j1, j2);
+                    System.out.println(duelo.iniciaDuelo());
+
+                    System.out.println(j1);
+                    System.out.println(j2);
+                  
+                    break;
+                case 0:
+                    opcao = 0;
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
+        } while (opcao != 0);
 
     }
 
